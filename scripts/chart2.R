4cd80bf13a517fabfca620d82d85f39fee62753d
glory-yang <gyang2@uw.edu>
2020-05-19 21:38:10 -0700

    complete chart2

---
+++ scripts/chart2.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+df <- read.csv("../data/data.csv", stringsAsFactors = FALSE, fileEncoding = "UTF-8-BOM")
+
+epr_co2_df <- df %>% 
+  mutate(avg_epr = rowMeans(select(df,epr_female, epr_male), na.rm = TRUE)) %>%
+  filter(co2_emissions != "..") %>% 
+  mutate(co2_emissions_num = as.numeric(co2_emissions)) %>% 
+  select(co2_emissions_num, avg_epr) %>% 
+  drop_na()
+
+ggplot(data = epr_co2_df) +
+  geom_point(mapping = aes(x = avg_epr, y = co2_emissions_num), alpha = 0.3) +
+  labs(
+    title = "Employment to Population Ratio vs Co2 Emissions per capita",
+    x = "Employment to Population Ratio (avg of male and female)",
+    y = "Co2 Emissions per capita"
+  )
