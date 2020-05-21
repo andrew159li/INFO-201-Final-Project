03a416a2b344558df83922bb564ec416d764479e
Alvin Jeong <alvinjeong.98@gmail.com>
2020-05-21 01:06:54 -0700

    added units of each columns to as the first row

--- scripts/table.R
+++ scripts/table.R
@@ -5,6 +5,7 @@ data <- read.csv("../data/data.csv",
                  stringsAsFactors = FALSE)
 data[, 5:9] <- suppressWarnings(sapply(data[, 5:9], as.numeric))
 
+# Aggregate table data
 table_1991_2019 <- data %>%
   select(year, country, income_pc, edu_spending, 
          epr_female, epr_male, co2_emissions) %>%
@@ -15,6 +16,20 @@ table_1991_2019 <- data %>%
             Average_Male_Employment = mean(epr_male, na.rm = T),
             Average_CO2_Emissions = mean(co2_emissions, na.rm = T)) %>%
   arrange(-Average_Income)
-  
 
+# Make a row(single-row dataframe containing units of each columns)
+units <- data.frame(country = "Full country name",
+                    Average_Income = "Current USD",
+                    Averag_Education_Expenditure = 
+                      "% of total expenditure in public institutions",
+                    Average_Female_Employment = 
+                      "Employment to population ratio, female of age 15+",
+                    Average_Male_Employment = 
+                      "Employment to population ratio, male of age 15+",
+                    Average_CO2_Emissions = "Metric tons per capita")
 
+# Converting the table dataframe into characters 
+# in order to concatenate it with the units dataframe.
+table_1991_2019[] <- lapply(table_1991_2019, as.character)
+
+table_data <- bind_rows(units, table_1991_2019)  
