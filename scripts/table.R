4c152e0a03555bbbe2843e042eea96ab5080f064
Alvin Jeong <alvinjeong.98@gmail.com>
2020-05-21 17:59:23 -0700

    changed table to incorporate new data and delete old ones.

--- scripts/table.R
+++ scripts/table.R
@@ -3,15 +3,17 @@ library("dplyr")
 data <- read.csv("../data/data.csv",
                  fileEncoding = "UTF-8-BOM",
                  stringsAsFactors = FALSE)
-data[, 5:9] <- suppressWarnings(sapply(data[, 5:9], as.numeric))
+data[, 5:11] <- suppressWarnings(sapply(data[, 5:11], as.numeric))
 
 # Aggregate table data
 table_1991_2019 <- data %>%
-  select(year, country, income_pc, edu_spending, 
-         epr_female, epr_male, co2_emissions) %>%
+  select(year, country, income_pc, population, pupils_pri_educ, 
+         pupils_sec_educ, epr_female, epr_male, co2_emissions) %>%
   group_by(country) %>%
   summarise(Average_Income = mean(income_pc, na.rm = T),
-            Average_Education_Expediture = mean(edu_spending, na.rm = T),
+            Average_Population = mean(population, na.rm = T),
+            Average_Primary_Students = mean(pupils_pri_educ, na.rm = T),
+            Average_Secondary_Students = mean(pupils_sec_educ, na.rm = T),
             Average_Female_Employment = mean(epr_female, na.rm = T),
             Average_Male_Employment = mean(epr_male, na.rm = T),
             Average_CO2_Emissions = mean(co2_emissions, na.rm = T)) %>%
@@ -20,8 +22,11 @@ table_1991_2019 <- data %>%
 # Make a row(single-row dataframe containing units of each columns)
 units <- data.frame(country = "Full country name",
                     Average_Income = "Current USD",
-                    Averag_Education_Expenditure = 
-                      "% of total expenditure in public institutions",
+                    Average_Population = "Number of people",
+                    Average_Primary_Students = 
+                      "Number of students in primary education",
+                    Average_Secondary_Students = 
+                      "Number of students in secondary education",
                     Average_Female_Employment = 
                       "Employment to population ratio, female of age 15+",
                     Average_Male_Employment = 
