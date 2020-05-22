4d275409000ab63151bbcc03874c71e1c04955f9
s-y-l <sooylee@uw.edu>
2020-05-21 19:16:25 -0700

    debug error message; clear NAs

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -4,28 +4,31 @@ library("ggplot2")
 library("tidyr")
 
 # clean up data
-population_school_country <- function(df) {
+chart1 <- function(df) {
   df_clean <- df %>%
-  mutate(
-    primary_perc = pupils_pri_educ / population,
-    secondary_perc = pupils_sec_educ / population
-  ) %>%
-  select(country, primary_perc, secondary_perc) %>%
-  group_by(country) %>%
-  summarise(
-    avg_primary = mean(primary_perc, na.rm = TRUE),
-    avg_secondary = mean(secondary_perc, na.rm = TRUE)
-  )
+    mutate(
+      primary_perc = pupils_pri_educ / population,
+      secondary_perc = pupils_sec_educ / population
+    ) %>%
+    select(country, primary_perc, secondary_perc) %>%
+    group_by(country) %>%
+    summarise(
+      avg_primary = mean(primary_perc, na.rm = TRUE),
+      avg_secondary = mean(secondary_perc, na.rm = TRUE)
+    )
   # melt dataframe
   df_melt <- gather(df_clean,
     key = "school_type",
     value = "population_perc", -country
   )
+  # replace non-numeric data with 0
+  df_melt_no_na <- df_melt
+  df_melt_no_na[is.na(df_melt_no_na)] <- 0
   # create bar chart
-  pop_school_country_plot <- ggplot(data = df_melt) +
+  pop_school_country_plot <- ggplot(data = df_melt_no_na) +
     geom_col(
       mapping = aes(x = country, y = population_perc, fill = school_type),
-     position = "stack"
+      position = "stack"
     ) +
     coord_flip() +
     ggtitle("Population in School per Country, Average 1991-2019") +
