00279ffd79b118c19a9dea0a8061e2b1a955baaa
Catherine <coei26@uw.edu>
2020-05-21 19:29:03 -0700

    fix ordering for chart 3

--- scripts/chart3.R
+++ scripts/chart3.R
@@ -15,10 +15,15 @@ chart3 <- function(df) {
     mutate(co2_growth_rate =
              (co2_emissions[year == "2014"] - co2_emissions[year == "1992"]) /
              co2_emissions[year == "1992"] * 100) %>%
-    mutate(co2_growth_rate = round(co2_growth_rate, 2))
-  
+    mutate(co2_growth_rate = round(co2_growth_rate, 2)) %>%
+    arrange(-co2_growth_rate)
+
+  df2 <- plot_df
+  df2$country <- factor(df2$country,
+                      levels = unique(df2$country[order(df2$co2_growth_rate)]))
+
   # Chart
-  chart <- ggplot(plot_df2, aes(x = country,
+  chart <- ggplot(df2, aes(x = country,
                                 y = co2_growth_rate,
                                 fill = country)) +
     geom_bar(stat = "identity") +
@@ -29,7 +34,7 @@ chart3 <- function(df) {
          fill = "Countries") +
     theme(axis.text.x = element_text(angle = 90, hjust = 1),
           axis.text.y = element_text(hjust = 1))
-  
+
   interactive_chart <- ggplotly(chart = ggplot2::last_plot(),
                                 tooltip = "co2_growth_rate")
   return(interactive_chart)
