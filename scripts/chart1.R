5f16002255501f97ad1830aac7a32faa1fba411f
s-y-l <sooylee@uw.edu>
2020-05-21 18:16:54 -0700

    make chart1 into function

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -3,14 +3,9 @@ library("dplyr")
 library("ggplot2")
 library("tidyr")
 
-# load in data
-df <- read.csv("../data/data.csv",
-  stringsAsFactors = FALSE,
-  fileEncoding = "UTF-8-BOM"
-)
-
 # clean up data
-df_clean <- df %>%
+population_school_country <- function(df) {
+  df_clean <- df %>%
   mutate(
     primary_perc = pupils_pri_educ / population,
     secondary_perc = pupils_sec_educ / population
@@ -21,29 +16,29 @@ df_clean <- df %>%
     avg_primary = mean(primary_perc, na.rm = TRUE),
     avg_secondary = mean(secondary_perc, na.rm = TRUE)
   )
-
-# melt dataframe
-df_melt <- gather(df_clean,
-  key = "school_type",
-  value = "population_perc", -country
-)
-
-# bar chart
-population_school_country <- ggplot(data = df_melt) +
-  geom_col(
-    mapping = aes(x = country, y = population_perc, fill = school_type),
-    position = "stack"
-  ) +
-  coord_flip() +
-  ggtitle("Population in School per Country, Average 1991-2019") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  xlab("Country") +
-  ylab("% Population in School") +
-  scale_fill_discrete(
-    name = "Type of School",
-    labels = c("Primary School", "Secondary School")
-  ) +
-  scale_y_continuous(
-    labels = scales::percent_format(accuracy = 1),
-    limits = c(0, 0.3)
+  # melt dataframe
+  df_melt <- gather(df_clean,
+    key = "school_type",
+    value = "population_perc", -country
   )
+  # create bar chart
+  pop_school_country_plot <- ggplot(data = df_melt) +
+    geom_col(
+      mapping = aes(x = country, y = population_perc, fill = school_type),
+     position = "stack"
+    ) +
+    coord_flip() +
+    ggtitle("Population in School per Country, Average 1991-2019") +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    xlab("Country") +
+    ylab("% Population in School") +
+    scale_fill_discrete(
+      name = "Type of School",
+      labels = c("Primary School", "Secondary School")
+    ) +
+    scale_y_continuous(
+      labels = scales::percent_format(accuracy = 1),
+      limits = c(0, 0.3)
+    )
+  return(pop_school_country_plot)
+}
