d414fbee11eccc509ffed029bba431b3831aa471
Catherine <coei26@uw.edu>
2020-05-21 19:01:08 -0700

    update chart3

--- scripts/chart3.R
+++ scripts/chart3.R
@@ -5,7 +5,7 @@ library(lintr)
 
 chart3 <- function(df) {
   # Setting Up Data
-  df[, 5:9] <- suppressWarnings(sapply(data[, 5:9], as.numeric))
+  df[, 5:9] <- suppressWarnings(sapply(df[, 5:9], as.numeric))
 
   # Chart Data
   plot_df <- df %>%
@@ -16,20 +16,20 @@ chart3 <- function(df) {
              (co2_emissions[year == "2014"] - co2_emissions[year == "1992"]) /
              co2_emissions[year == "1992"] * 100) %>%
     mutate(co2_growth_rate = round(co2_growth_rate, 2))
-
+  
   # Chart
-  chart <- ggplot(plot_df, aes(x = country,
-                               y = co2_growth_rate,
-                               fill = country)) +
+  chart <- ggplot(plot_df2, aes(x = country,
+                                y = co2_growth_rate,
+                                fill = country)) +
     geom_bar(stat = "identity") +
     theme(plot.title = element_text(hjust = 0.5)) +
     labs(title = "CO2 Emission Growth Rates by Country from 1992 - 2014",
          x = "Country",
-         y = "CO2 Emissions Growth Rate (%)",
+         y = "CO2 Emission Growth Rate (%)",
          fill = "Countries") +
     theme(axis.text.x = element_text(angle = 90, hjust = 1),
           axis.text.y = element_text(hjust = 1))
-
+  
   interactive_chart <- ggplotly(chart = ggplot2::last_plot(),
                                 tooltip = "co2_growth_rate")
   return(interactive_chart)
